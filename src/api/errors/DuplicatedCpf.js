2c9bd47e8a9f98ebfd13fdf5a6f4e92f154b579c
Adson Sousa de Oliveira <adson.oliveira@compasso.com.br>
2022-06-23 14:57:06 -0300

    refactor: adjustments on error classes

--- src/api/errors/DuplicatedCpf.js
+++
@@ -1,9 +0,0 @@
-class DuplicatedCpf extends Error {
-    constructor(cpf) {
-        super(cpf);
-        this.name = 'Duplicated CPF';
-        this.description = `The CPF ${cpf} already exists`
-    }
-}
-
-module.exports = DuplicatedCpf;
